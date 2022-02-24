760fab75dd342f2f59610216afee6361610568fc
Wilson Leung <wilsonleung0914@gmail.com>
2022-02-25 01:27:57 +0800

    Initial Commit

---
+++ HKWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HKWeather
+//
+//  Created by Wilson Leung on 25/2/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HKWeather/HKWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKWeatherApp.swift
+//  HKWeather
+//
+//  Created by Wilson Leung on 25/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct HKWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
